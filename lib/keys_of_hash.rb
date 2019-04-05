5eee7b47ebe635a6b049ffcf64a34e3a38cf0daf
Michael Chen <michaelchengaming@gmail.com>
2019-04-05 20:47:58 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,17 @@
+require "pry"
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    new_hash = []
+    arguments.each do |arg|
+      each do |key, value|
+        #binding.pry
+        if value == arg
+          new_hash.push(key)
+        end
+      end
+    end
+    #binding.pry
+    new_hash
   end
 end
